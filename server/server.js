e3cc26c737d4897a3e45f560dadc5ff4258a350c
Dar2402 <darshanrpandey243@gmail.com>
2023-11-03 22:56:32 +0530

    first changes

--- server/server.js
+++ server/server.js
@@ -34,12 +34,10 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "..", "chatapp", "build")));
+  app.use(express.static(path.join(__dirname1, "/chatapp/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname1, "..", "chatapp", "build", "index.html")
-    )
+    res.sendFile(path.resolve(__dirname1, "chatapp", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
