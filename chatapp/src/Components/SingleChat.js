e6a53ca7b6e0dcad3f83046d2e7e48a164345665
Dar2402 <darshanrpandey243@gmail.com>
2023-11-03 23:23:26 +0530

    modified:   chatapp/src/Components/SingleChat.js

--- chatapp/src/Components/SingleChat.js
+++ chatapp/src/Components/SingleChat.js
@@ -12,7 +12,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from '../animations/typing.json'
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://connexa.onrender.com";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
